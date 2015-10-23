13fec04ab1fdd190533c9e09de697d2d9ec7e8ba
Ernesto Lowy <ernesto@ebi.ac.uk>
2015-10-23 14:01:23 +0100

    Added script to check if there are runs that are not in the merged_run_bam

---
+++ scripts/pipeline_run/check_if_merge_is_needed.pl
@@ -0,0 +1,68 @@
+use strict;
+use warnings;
+use ReseqTrack::DBSQL::DBAdaptor;
+use Getopt::Long;
+use ReseqTrack::Tools::Exception;
+use Data::Dumper;
+use Array::Utils qw(:all);
+
+my ( $dbhost, $dbuser, $dbpass, $dbport, $dbname );
+
+&GetOptions(
+    'dbhost=s'                  => \$dbhost,
+    'dbname=s'                  => \$dbname,
+    'dbuser=s'                  => \$dbuser,
+    'dbpass=s'                  => \$dbpass,
+    'dbport=s'                  => \$dbport);
+
+# dba + adaptors
+my $db = ReseqTrack::DBSQL::DBAdaptor->new(
+    -host   => $dbhost,
+    -user   => $dbuser,
+    -port   => $dbport,
+    -dbname => $dbname,
+    -pass   => $dbpass,
+);
+throw("Could not create db adaptor") if ( !$db );
+
+$db->dbc->disconnect_when_inactive(1);
+my $ca   = $db->get_CollectionAdaptor;
+my $rm   = $db->get_RunMetaInfoAdaptor;
+
+my %hash;
+
+my $type_merge='CHIP_MERGE_RUN_BAM';
+my $cs = $ca->fetch_by_type( $type_merge);
+
+foreach my $c ( @{$cs} ){
+    throw("Collection $type_merge does not contain files") unless ( $c->table_name eq 'file' );
+    
+    my $experiment_name=$c->name;
+    my $other_ids=$c->other_ids;
+    foreach my $other_id (@$other_ids) {
+	my $collectionArrays=$ca->fetch_by_other_id_and_type($other_id,'CHIP_RUN_BAM');
+	foreach my $collection (@$collectionArrays) {
+	    $hash{$experiment_name}{'merged'}{$collection->name}=0;
+	}
+    }   
+}
+
+foreach my $ex (keys %hash) {
+    my $run_meta_info_array=$rm->fetch_by_experiment_id($ex);
+    foreach my $rminfo (@$run_meta_info_array) {
+	my $runid=$rminfo->run_id;
+	#get run ids
+	my $collection=$ca->fetch_by_name_and_type($runid,'CHIP_RUN_BAM');
+	next if !$collection;
+	$hash{$ex}{'new'}{$collection->name}=0;
+    }
+}
+
+foreach my $ex (keys %hash) {
+    my @merged=keys %{$hash{$ex}{'merged'}};
+    my @new=keys %{$hash{$ex}{'new'}};
+    if ( array_diff(@merged, @new) ) {
+	print "[HEY!] For experiment $ex: runs_merged:@merged total_runs:@new\n";
+    }
+}
+
