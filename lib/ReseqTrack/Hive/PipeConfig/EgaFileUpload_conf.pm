346b9d9fbdb275543e779a25979f5b4908c624ff
avikdatta <avikd@ebi.ac.uk>
2016-03-22 15:06:18 +0000

    added aspera upload

--- lib/ReseqTrack/Hive/PipeConfig/EgaFileUpload_conf.pm
+++ lib/ReseqTrack/Hive/PipeConfig/EgaFileUpload_conf.pm
@@ -27,10 +27,13 @@ sub default_options {
 
         'pipeline_name' => 'ega_upload',                   # name used by the beekeeper to prefix job names on the farm
 
-            # runnable-specific parameters' defaults:
-        'java_path'  => 'java',
-        'ega_jar'     => 'webin-data-streamer-Upload-Client.jar',
-        'file'            => undef,
+        # runnable-specific parameters' defaults:
+        'java_path'   => 'java',
+        'ega_jar'     => undef,
+        'file'        => undef,
+        'work_dir'    => undef,
+        'upload_dest' => undef,
+        'lsf_queue'   => 'production',
     };
 }
 
@@ -40,8 +43,17 @@ sub pipeline_create_commands {
     return [
         @{$self->SUPER::pipeline_create_commands},  # inheriting database and hive tables' creation
 
-       # 'mkdir -p '.$self->o('work_dir'),
+        'mkdir -p '.$self->o('work_dir'),
     ];
+}
+
+sub resource_classes {
+  my ($self) = @_;
+    return {
+          %{$self->SUPER::resource_classes}, 
+          '200Mb' => { 'LSF' => '-C0 -M200 -q '.$self->o('lsf_queue').' -R"select[mem>200] rusage[mem=200]"' },
+          '500Mb' => { 'LSF' => '-C0 -M500 -q '.$self->o('lsf_queue').' -R"select[mem>500] rusage[mem=500]"' },
+    };
 }
 
 sub hive_meta_table {
@@ -70,8 +82,12 @@ sub pipeline_analyses {
         {   -logic_name => 'encrypt_file',
             -module        => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -parameters    => {
+                'java_path' => $self->o('java_path'),
+                'ega_jar'   => $self->o('ega_jar'),
                 'cmd'       => '#java_path# -jar #ega_jar# -file #filename#',
             },
+            -analysis_capacity => 10,
+            -rc_name => '500Mb',
             -flow_into => {
             1 => [ 'find_encrypted_files' ],
             },
@@ -80,7 +96,7 @@ sub pipeline_analyses {
         {    -logic_name => 'find_encrypted_files',
              -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
              -parameters => {  
-                     'inputcmd'      => "find #filename#.*",
+                     'inputcmd'      => 'find #filename#.*',
                      'column_names' => [ 'gpg_file' ],
              },
                -flow_into => {
@@ -96,22 +112,26 @@ sub pipeline_analyses {
                 'fan_branch_code' => 1,
             },
             -flow_into => {
-                1  =>  [ 'upload_file' ],  
+                1  =>  { 'upload_file' => { 'gpg_filename' => '#gpg_filename#', 'gpg_suffix' => '#expr( (#gpg_filename# =~ /\S+\/(\S+)$/)[0] )expr#' }},  
                 },   
         },
-         {   -logic_name => 'upload_file',
-             -module        => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-             -parameters    => {
-                'cmd'       => 'ls #gpg_filename#',
-            },
-            -flow_into => {
-                1  =>  [ 'clean_file' ],  
-                },   
+         {    -logic_name => 'upload_file',
+              -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+              -parameters    => {
+                  'work_dir' => $self->o('work_dir'),
+                  'upload_dest' => $self->o('upload_dest'),
+                  'cmd'       => 'mkdir -p #work_dir#/#gpg_suffix#;rm -f #work_dir#/#gpg_suffix#/aspera-scp-transfer.log;ascp -d -k2  -Tr -Q -l 100M -L #work_dir#/#gpg_suffix# #gpg_filename# #upload_dest#/#gpg_suffix#; cat #work_dir#/#gpg_suffix#/aspera-scp-transfer.log|perl -e \'while(<>){if(/LOG - Source file transfers passed\s+:\s+(\d)/){ die unless $ 1 > 0}}\'',
+              },
+              -rc_name => '500Mb',
+              -flow_into => {
+                 1  =>  { 'clean_file' => { 'gpg_filename' => '#gpg_filename#', 'gpg_suffix' => '#gpg_suffix#'} }, 
+             },
          },
          {   -logic_name => 'clean_file',
              -module        => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
              -parameters    => {
-                'cmd'       => 'rm -f #gpg_filename#',
+                'work_dir' => $self->o('work_dir'),
+                'cmd'       => 'rm -f #gpg_filename#; rm -rf #work_dir#/#gpg_suffix#',
             },
         },
     ];
