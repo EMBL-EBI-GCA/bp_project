4898170be1af8a30d8b4e8c089dbd48275a537a1
Ernesto Lowy <ernesto@ebi.ac.uk>
2016-04-14 10:55:52 +0100

    adding script to get samples form ERA

---
+++ scripts/xml/getListOfSamplesFromERA.pl
@@ -0,0 +1,54 @@
+#get list of samples from ERA and print them out without the extra <SAMPLE_SET> and newlines
+#in the middle
+use strict;
+use warnings;
+use ReseqTrack::Tools::ERAUtils;
+use utf8;
+use autodie;
+use Data::Dumper;
+use XML::Twig;
+
+my $sample_id_file = $ARGV[0];
+die("[usage] perl $0 sample.ids") if !$sample_id_file;
+
+my $erauser = $ENV{'ERA_USER'};
+my $erapwd = $ENV{'DB_PASS'};
+
+my $sample_id_list = get_list($sample_id_file);
+
+my @era_conn = ( $erauser, $erapwd );
+my $era = get_erapro_conn(@era_conn);
+$era->dbc->db_handle->{LongReadLen} = 66000;
+
+my $twig = XML::Twig->new(
+    twig_roots => 
+    { 'SAMPLE'=>\&process_sample },
+    pretty_print             => 'indented',
+    );
+
+my $xml_sth = $era->dbc->prepare("select xmltype.getclobval(sample_xml) xml from sample where sample_alias = ?");
+
+foreach my $sample_id ( @{ $sample_id_list } ) {
+    $xml_sth->execute( $sample_id );
+    my $xr = $xml_sth->fetchrow_arrayref();
+    my ($xml) = @$xr;
+    $twig->parse($xml);
+}
+
+sub process_sample {
+    my ($twig,$sample)=@_;
+    $sample->print();
+}
+
+sub get_list{
+    my ($file) = @_;
+    my @id_list;
+
+    open my $fh ,'<', $file;
+    while ( <$fh> ) {
+	chomp;
+	push (@id_list, $_);
+    }
+    close( $fh );
+    return \@id_list;
+}
